f688f2a1bbc34a5c60fd8fdc8670dce92b70e1cf
Zachary Shirmohammadli <zachshirow@gmail.com>
2024-10-08 17:01:01 +0330

    my first commit

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -36,7 +36,9 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           <LivePreviewListener />
 
           <Header />
+          <div className='main'>
           {children}
+          </div>
           <Footer />
         </Providers>
       </body>
